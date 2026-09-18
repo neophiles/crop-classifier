e4fc98c735c605d82c68c71a8218d73dc144f51b
neophiles <mnv.tagle@gmail.com>
2026-09-18 22:37:18 +0800

    setup scaffold and initial project structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="h-screen flex flex-col justify-center items-center">
+      <h1>Crops Classifier</h1>
+      <h2>Wahoo</h2>
+    </div>
+  )
+}
+
+export default App
